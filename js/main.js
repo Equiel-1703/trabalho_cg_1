f1d15cbdb29500c94bee327e3c387e83d8b6373e
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2025-01-27 18:34:55 -0300

    Added thumbsticks to control camera

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import OutputLog from "./OutputLog.js";
-import {Color, WebGLUtils} from "./WebGLUtils.js";
+import { Color, WebGLUtils } from "./WebGLUtils.js";
 import FileLoader from "./FileLoader.js";
 import GraphicsMath from "./GraphicsMath.js";
 import Vec4 from "./Vec4.js";
@@ -47,22 +47,22 @@ function renderCallBack(wgl_utils, gl, program, clear_color, camera, start) {
     const camera_matrix = camera.getCameraMatrix();
     const camera_uniform = gl.getUniformLocation(program, 'u_camera_matrix');
     gl.uniformMatrix4fv(camera_uniform, false, camera_matrix);
-    
+
     wgl_utils.clearCanvas(clear_color, gl);
-    
+
     for (let m in models_to_render) {
         const model = models_to_render[m];
 
         const objects = model.getRenderableObjects(); // Get renderable objects from model
         const transformation_matrix = model.getTransformationMatrix(); // Get transformation matrix from model
-        
+
         // Set transformation matrix (since it's the same for all objects, we can set it here)
         const transformation_uniform = gl.getUniformLocation(program, 'u_model_matrix');
         gl.uniformMatrix4fv(transformation_uniform, false, transformation_matrix);
 
         for (let o in objects) {
             let obj = objects[o];
-            
+
             // Set object VAO
             gl.bindVertexArray(obj.vao);
 
@@ -73,14 +73,14 @@ function renderCallBack(wgl_utils, gl, program, clear_color, camera, start) {
 
     let end = performance.now();
     const elapsed = end - start;
-    
+
     const diff = fps_limiter - elapsed;
-    
+
     // Update FPS counter in HTML
     document.getElementById('fps_counter').innerText = `FPS: ${Math.round(1000 / (elapsed + diff))}`;
 
     let start_2 = performance.now();
-    
+
     const callback = renderCallBack.bind(null, wgl_utils, gl, program, clear_color, camera, start_2);
 
     if (diff > 0) {
@@ -90,7 +90,72 @@ function renderCallBack(wgl_utils, gl, program, clear_color, camera, start) {
     } else {
         requestAnimationFrame(callback, start_2);
     }
+
+}
+
+function initializeThumbsticks(log) {
+    const thumb_cam_rot = document.getElementById('thumbstick_camera_rotation');
+
+    let moving = false;
+    let starting_pos = { x: 0, y: 0 };
+    let starting_transform = { x: 0, y: 0 };
+    const transform_limit = 35;
+    
+    const thumb_btn = thumb_cam_rot.getElementsByClassName('thumb_btn')[0];
+
+    // Helper functions
+    const set_thumb_translation = (x, y) => {
+        thumb_btn.style.transform = `translate(${x}px, ${y}px)`;
+    };
+    const get_thumb_translation = () => {
+        if (thumb_btn.style.transform.length > 0) {
+            let transform_content = thumb_btn.style.transform.split('(')[1].split(')')[0].split(',');
+            return { x: parseFloat(transform_content[0]), y: parseFloat(transform_content[1]) };
+        } else {
+            return { x: 0, y: 0 };
+        }
+    };
+
+    thumb_btn.addEventListener('mousedown', (e) => {
+        starting_pos = { x: e.x, y: e.y };
+        starting_transform = get_thumb_translation();
+
+        log.log(`Starting transform: ${starting_transform.x}, ${starting_transform.y}`);
+        log.log(`Starting position: ${starting_pos.x}, ${starting_pos.y}`);
         
+        moving = true;
+    });
+
+    document.addEventListener('mouseup', () => {
+        if (moving) {
+
+        }
+        moving = false;
+    });
+
+    document.addEventListener('mousemove', (e) => {
+        if (moving) {
+            const diff_x = e.x - starting_pos.x;
+            const diff_y = e.y - starting_pos.y;
+
+            let new_transform_x = starting_transform.x + diff_x;
+            let new_transform_y = starting_transform.y + diff_y;
+
+            if (Math.abs(new_transform_x) > transform_limit) {
+                new_transform_x = Math.sign(new_transform_x) * transform_limit;
+            }
+
+            if (Math.abs(new_transform_y) > transform_limit) {
+                new_transform_y = Math.sign(new_transform_y) * transform_limit;
+            }
+
+            set_thumb_translation(new_transform_x, new_transform_y);
+
+        } else {
+            return;
+        }
+    });
+
 }
 
 const FPS = 60;
@@ -98,6 +163,10 @@ let models_to_render = [];
 
 async function main() {
     const log = initializeLog();
+
+    initializeThumbsticks(log);
+
+    return;
 
     // Initializing FileLoader and WebGLUtils
     const fl = new FileLoader(log);
@@ -107,7 +176,7 @@ async function main() {
     const canvas = document.getElementById('glcanvas');
     const gl = wgl_utils.initializeWebGLContext(canvas);
 
-    
+
     // Loading shaders code
     const v_shader = await fl.loadShader('shaders/VertexShader.glsl');
     const f_shader = await fl.loadShader('shaders/FragmentShader.glsl');
@@ -117,11 +186,11 @@ async function main() {
     // Creating shaders
     const vertex_shader = wgl_utils.createShader(gl.VERTEX_SHADER, v_shader);
     const fragment_shader = wgl_utils.createShader(gl.FRAGMENT_SHADER, f_shader);
-    
+
     if (!vertex_shader || !fragment_shader) {
         throw new Error('Failed to create shaders.');
     }
-    
+
     log.success_log('main> Shaders created.');
 
     // Creating program
@@ -132,7 +201,7 @@ async function main() {
     }
 
     gl.useProgram(program);
-    
+
     log.success_log('main> Program created.');
 
     // Creating perspective matrix
